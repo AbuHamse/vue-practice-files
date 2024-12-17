2968f8f235d88a5ed99b70caa305c9aa942212b0
AbuHamse <dirie.a99@gmail.com>
2024-12-17 11:06:30 -0500

    Meh

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,10 @@ const router = createRouter({
     routes: [{
         path:'/',
         name:'home',
-       /*Component Suppose to go through here */
+       /*Component Suppose to go through here, then in the main app component you have to export the 
+       the router then after doing so you have to import it in the main app file and the use app.use(router) */
     }]
 
 })
+
+export default router 
